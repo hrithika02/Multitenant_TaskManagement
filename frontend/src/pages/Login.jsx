400d5e45d733570068b2a46f94a64b7142cbad34
hrithika02 <hrithivishva.tnj@gmail.com>
2026-04-14 14:44:04 +0530

    updated1

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import API from "../services/api";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 
 function Login() {
     const [email, setEmail] = useState("");
