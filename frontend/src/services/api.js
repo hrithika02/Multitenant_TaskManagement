d1eab13e7898eab7b2675e89446dba83e68ae893
hrithika02 <hrithivishva.tnj@gmail.com>
2026-04-14 13:30:26 +0530

    fixed API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "hhttps://multitenant-taskmanagement-1.onrender.com/api"
 });
 
 // Attach token automatically
